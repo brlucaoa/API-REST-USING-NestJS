4ad0a028c00bb75c0258a52dfc5d8817a912856d
Lucas Gomes <204907@upf.br>
2025-11-23 18:08:12 -0300

    REMOVE HEALTHCHECK

--- health-api/dist/common/interceptors/logging.interceptor.d.ts
+++
@@ -1,8 +0,0 @@
-import { NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
-import { Observable } from 'rxjs';
-import { MyLoggerService } from '../logger/my-logger.service';
-export declare class LoggingInterceptor implements NestInterceptor {
-    private logger;
-    constructor(logger: MyLoggerService);
-    intercept(context: ExecutionContext, next: CallHandler): Observable<any>;
-}
